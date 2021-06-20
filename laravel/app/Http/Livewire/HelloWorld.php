6cb98a7111cb48f75297957afa2f7eab762dfec5
JJ-Lewis <lewis123jam@gmail.com>
2021-06-20 07:17:01 +0200

    formatting and render changes

--- laravel/app/Http/Livewire/HelloWorld.php
+++ laravel/app/Http/Livewire/HelloWorld.php
@@ -8,11 +8,29 @@ use App\Api\Account;
 
 class HelloWorld extends Component
 {
+    public $address = '0x14B1B9A7cdaA2A917e19449FeBe6462602864226';
+    // get bsc client
     private $account;
+    // calculator
+    private $formattedTxns;
+    public $currencyList = [];
+    public $lgDateFrom = '';
+    public $lgDateTo = '';
+    public $taxDateFrom = '';
+    public $taxDateTo = '';
+
 
     public function mount(Account $account)
     {
         $this->account = $account;
+        // format transactions into array of arrays
+        foreach ($this->account->getBscTxnList() as $key => $txn) {
+            $newTxn = [];
+            foreach ($txn as $key => $field) {
+                $newTxn[$key] = $field;
+            }
+            $this->formattedTxns[] = $newTxn;
+        }
     }
 
     public function render()
@@ -22,6 +40,16 @@ class HelloWorld extends Component
 
     private function getTxns()
     {
-        return $this->account->getBscTxnList();
+        return collect($this->formattedTxns);
+    }
+
+    public function calculateLossGain()
+    {
+        // get the to and from dates, and filter the transactions by it
+
+        // if there are transactions in that time period, calculate
+
+        // otherwise return with error
+        return;
     }
 }
--- laravel/resources/views/livewire/hello-world.blade.php
+++ laravel/resources/views/livewire/hello-world.blade.php
@@ -12,10 +12,20 @@
                         @foreach ($txns as $key => $txn)
                             <div class="list-group-item">
                                 <div class="d-flex w-100 justify-content-between">
-                                   <span>Transaction Number: {{ $key }}</span>
-                                   <span>{{ Carbon\Carbon::createFromTimestamp($txn->{'timeStamp'})->toDateTimeString() }}</span>
+                                   <span>Nonce: {{ $txn['nonce'] }}</span>
+                                   <span>Timestamp: {{ Carbon\Carbon::createFromTimestamp($txn['timeStamp'])->toDateTimeString() }}</span>
                                 </div>
-                                <p></p>
+                                <div class="d-flex w-100 justify-content-between">
+                                    <span>Value: {{ floatval($txn['value'])/pow(10, 18) }} BNB</span>
+                                 </div>
+                                 <div class="d-flex w-100 justify-content-between">
+                                     @php
+                                         $from = ($txn['from'] === strtolower($address)) ? 'me' : $txn['from'];
+                                         $to = ($txn['to'] === strtolower($address)) ? 'me' : $txn['to'];
+                                     @endphp
+                                    <span>From: {{ $from }}</span>
+                                    <span>To: {{ $to }}</span>
+                                 </div>
                             </div>
                         @endforeach
                     </div>
@@ -25,7 +35,35 @@
             
         </div>
         <div class="col-6">
-            <h2>Calculator</h2>
+            <div class="row">
+                <div class="col-12">
+                    <h2>Calculator</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-6">
+                    <h4>Calculate LOSS/GAIN</h4>
+                    <div class="form-group mb-4">
+                        <label for="lossgainDateFrom">Date From</label>
+                        <input wire:model.defer="lgDateFrom" type="date" class="form-control" name="lossgainDateFrom">
+                        <span>{{ $lgDateFrom }}</span>
+                        <label for="lossgainDateTo">Date To</label>
+                        <input wire:model.defer="lgDateTo" type="date" class="form-control" name="lossgainDateTo">
+                        <span>{{ $lgDateTo }}</span>
+                    </div>
+                    <button class="btn btn-xl btn-primary" wire:click="calculateLossGain">LOSS/GAIN ME</button>
+                </div>
+                <div class="col-6">
+                    <h4>Calculate Taxable Income</h4>
+                    <div class="form-group mb-4">
+                        <label for="taxDateFrom">Date From</label>
+                        <input wire:model.defer="taxDateFrom" type="date" class="form-control" name="taxDateFrom">
+                        <label for="taxDateTo">Date To</label>
+                        <input wire:model.defer="taxDateTo" type="date" class="form-control" name="taxDateTo">
+                    </div>
+                    <button class="btn btn-xl btn-primary">TAX ME</button>
+                </div>
+            </div>
         </div>
     </div>
 </div>
